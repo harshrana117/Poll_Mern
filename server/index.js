07910624a852c1738ab0552db6b62cc92d91b93a
Harsh Rana <harshrana117@gmail.com>
2022-08-23 20:47:19 +0530

    Initial commit and express and routes setup

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const handle = require('./handlers')
+
+
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => res.send("Hello world"));
+
+
+app.use(handle.notFound);
+app.use(handle.errors);
+
+app.listen(port, console.log('server started'));
